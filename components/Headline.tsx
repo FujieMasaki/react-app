5681ee0bf14ddf3c283a96bdae419430e05c145e
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-11-15 23:13:41 +0900

    ♻️ Mainコンポーネント作成しchildrenを使用

--- components/Headline.tsx
+++ components/Headline.tsx
@@ -1,16 +1,24 @@
 import Image from "next/image";
+import { ReactNode } from "react";
 import styles from "../styles/Home.module.css";
 
 type Headline = {
   page: string;
+  children: ReactNode;
+};
+
+type Obj = {
+  foo: string;
+  bar: string;
 };
 export function Headline(props: Headline) {
+  console.log(props);
   return (
     <div>
       <h1 className={styles.title}>{props.page} page</h1>
       <p className={styles.description}>
-        Get started by editing{" "}
-        <code className={styles.code}>pages/{props.page}.tsx</code>
+        Get started by editing
+        {props.children}
       </p>
     </div>
   );
