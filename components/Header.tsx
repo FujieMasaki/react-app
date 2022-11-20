ae3c03e66caeca7d35b12089a160eab3391b6a11
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-11-21 06:23:20 +0900

    💄 styleの追加

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
+import classes from "./Header.module.css";
 
 export function Header() {
   return (
-    <header>
-      <Link href="/">Index</Link>
-      <Link href="/about">About</Link>
+    <header className={classes.header}>
+      <Link href="/" className={classes.anchor}>
+        Index
+      </Link>
+      <Link href="/about" className={classes.anchor}>
+        About
+      </Link>
     </header>
   );
 }
