d94bfee97406dcb2a7236707c0859af2009c4e8c
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-12-01 23:09:43 +0900

    ✨ eventを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,23 @@ import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 
-import styles from "src/style/Home.module.css";
+import styles from "src/styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <title>Create Next App</title>
       <Header />
+      {/* eにはeventをクリックした時の要素が入る */}
+      <a
+        href="/about"
+        onClick={function (e) {
+          console.log(e.target);
+          e.preventDefault();
+        }}
+      >
+        ボタン
+      </a>
 
       <Main page={"index"} />
 
