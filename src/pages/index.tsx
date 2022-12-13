70566e2c489e88cd2aa82f022ae0bfb8b4603105
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-12-13 22:47:03 +0900

    ⚡️ 第二引数を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,22 @@ import Image from "next/image";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 import styles from "src/styles/Home.module.css";
 
 export default function Home() {
   const [count, setCount] = useState(1);
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-  };
+
+  const handleClick = useCallback(
+    (e) => {
+      console.log(count);
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+    },
+    [count]
+  );
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
